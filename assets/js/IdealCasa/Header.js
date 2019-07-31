98fd91a17aca4c86e4ed0dc45947d1e12995a4b2
Cynthiaaa <cynthia.verger@gmail.com>
2019-07-31 14:57:18 +0200

    fixed issues dependencies

---
+++ assets/js/IdealCasa/Header.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+
+export default class Header extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
+}
+render () {
+    return ( <div>hello</div>)
+};
+
---
+++ assets/js/IdealCasa/IdealCasa.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+    constructor() {
+        super()
+    }
+
+render () {
+    return ( <div><Header /></div>)
+}
+}
+
+ReactDOM.render(<App />, app)
